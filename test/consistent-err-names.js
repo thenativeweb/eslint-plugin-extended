fa47c9713fb3bbc93a7fa467925b1abfb1fe82b3
Golo Roden <webmaster@goloroden.de>
2015-08-04 14:27:33 +0200

    Initial commit.

---
+++ test/consistent-err-names.js
@@ -0,0 +1,36 @@
+'use strict';
+
+var RuleTester = require('eslint').RuleTester;
+
+var rule = require('../lib/consistent-err-names');
+
+var ruleTester = new RuleTester();
+
+ruleTester.run('consistent-err-names', rule, {
+  valid: [
+    { code: '(function (err) {})()' },
+    { code: '(function (err) {})()', options: [ 'prefix' ]},
+    { code: '(function (err) {})()', options: [ 'suffix' ]},
+    { code: '(function (error) {})()' },
+    { code: '(function (error) {})()', options: [ 'prefix' ]},
+    { code: '(function (error) {})()', options: [ 'suffix' ]},
+    { code: '(function (roerr) {})()' },
+    { code: '(function (roerr) {})()', options: [ 'prefix' ]},
+    { code: '(function (roerr) {})()', options: [ 'suffix' ]},
+    { code: '(function (errFoo) {})()' },
+    { code: '(function (errFoo) {})()', options: [ 'prefix' ]},
+    { code: '(function (fooErr) {})()', options: [ 'suffix' ]},
+    { code: '(function (foobar) {})()' },
+    { code: '(function (foobar) {})()', options: [ 'prefix' ]},
+    { code: '(function (foobar) {})()', options: [ 'suffix' ]},
+    { code: '(function (hyperreduced) {})()' },
+    { code: '(function (hyperreduced) {})()', options: [ 'prefix' ]},
+    { code: '(function (hyperreduced) {})()', options: [ 'suffix' ]}
+  ],
+
+  invalid: [
+    { code: '(function (barErr) {})()', errors: [{ message: 'Invalid error name.' }]},
+    { code: '(function (barErr) {})()', errors: [{ message: 'Invalid error name.' }], options: [ 'prefix' ]},
+    { code: '(function (errBar) {})()', errors: [{ message: 'Invalid error name.' }], options: [ 'suffix' ]}
+  ]
+});
